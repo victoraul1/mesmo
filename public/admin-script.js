6f6d1636eab9913cadb4506f52be51a033d368ed
Victor Galindo <victorgalindo475@gmail.com>
2024-05-19 11:26:13 -0700

    Updated project structure and files for responsive grid

--- public/admin-script.js
+++ public/admin-script.js
@@ -1,7 +1,24 @@
 const socket = io();
 
 const quill = new Quill('#editor', {
-    theme: 'snow'
+    theme: 'snow',
+    modules: {
+        toolbar: [
+            [{ 'header': [1, 2, false] }],
+            ['bold', 'italic', 'underline'],
+            ['blockquote', 'code-block'],
+            [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+            [{ 'script': 'sub'}, { 'script': 'super' }],
+            [{ 'indent': '-1'}, { 'indent': '+1' }],
+            [{ 'direction': 'rtl' }],
+            [{ 'size': ['small', false, 'large', 'huge'] }],
+            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+            [{ 'color': [] }, { 'background': [] }],
+            [{ 'font': [] }],
+            [{ 'align': [] }],
+            ['clean']
+        ]
+    }
 });
 
 socket.on('contentUpdate', (data) => {
