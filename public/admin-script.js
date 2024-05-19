93d7bfdd7e3d58b5e10640226197b5273d060d73
Victor Galindo <victorgalindo475@gmail.com>
2024-05-19 16:41:15 -0700

    Updated project structure and files for responsive grid

--- public/admin-script.js
+++ public/admin-script.js
@@ -1,7 +1,23 @@
 const socket = io();
 
 const quill = new Quill('#editor', {
-    theme: 'snow'
+    theme: 'snow',
+    modules: {
+        toolbar: {
+            container: [
+                [{ 'header': [1, 2, false] }],
+                ['bold', 'italic', 'underline'],
+                [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+                ['link', 'image'],
+                [{ 'align': [] }],
+                ['clean'],
+                ['html']  // Custom button for HTML editing
+            ],
+            handlers: {
+                'html': showHtmlEditor
+            }
+        }
+    }
 });
 
 socket.on('contentUpdate', (data) => {
@@ -10,34 +26,30 @@ socket.on('contentUpdate', (data) => {
 
 document.getElementById('save-button').addEventListener('click', () => {
     let content = quill.root.innerHTML;
-    content = formatContent(content);
     socket.emit('save', { content: content });
 });
 
-function formatContent(content) {
-    const tempDiv = document.createElement('div');
-    tempDiv.innerHTML = content;
-    const items = tempDiv.querySelectorAll('p');
+function showHtmlEditor() {
+    const modal = document.getElementById('html-modal');
+    const htmlEditor = document.getElementById('html-editor');
+    const content = quill.root.innerHTML;
 
-    items.forEach(item => {
-        const priceMatch = item.textContent.match(/(S\/\.\d+(\.\d{2})?)/g);
-        if (priceMatch) {
-            priceMatch.forEach(price => {
-                const priceSpan = `<span class="price">${price}</span>`;
-                item.innerHTML = item.innerHTML.replace(price, priceSpan);
-            });
-            item.classList.add('menu-item');
-        } else {
-            item.classList.add('menu-item');
+    htmlEditor.value = content;
+    modal.style.display = "block";
+
+    document.getElementById('save-html-button').onclick = function() {
+        const newContent = htmlEditor.value;
+        quill.clipboard.dangerouslyPasteHTML(newContent);
+        modal.style.display = "none";
+    };
+
+    document.querySelector('.close').onclick = function() {
+        modal.style.display = "none";
+    };
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
         }
-
-        // Check for categories (in bold)
-        if (item.innerText === item.innerHTML.toUpperCase() && !item.querySelector('.price')) {
-            item.style.fontWeight = 'bold';
-            item.style.marginTop = '1em';
-            item.style.marginBottom = '0.5em';
-        }
-    });
-
-    return tempDiv.innerHTML;
+    };
 }
