01d6056bcf0d892cc25575f0160cfb59d2841f14
Victor Galindo <victorgalindo475@gmail.com>
2024-05-19 17:26:08 -0700

    Updated project structure and files for responsive grid

--- public/admin-script.js
+++ public/admin-script.js
@@ -1,47 +1,30 @@
 const socket = io();
 
 const quill = new Quill('#editor', {
-    theme: 'snow',
-    modules: {
-        toolbar: {
-            container: "#toolbar",
-            handlers: {
-                'code': showHtmlEditor  // Custom button handler
-            }
-        }
-    }
+  theme: 'snow',
+  modules: {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ['bold', 'italic', 'underline'],
+      ['link', 'image'],
+      [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+      ['code-block'] // Opción para código HTML
+    ]
+  }
 });
 
-socket.on('contentUpdate', (data) => {
-    quill.clipboard.dangerouslyPasteHTML(data.content);
-});
-
-document.getElementById('save-button').addEventListener('click', () => {
-    let content = quill.root.innerHTML;
-    socket.emit('save', { content: content });
-});
-
-function showHtmlEditor() {
-    const modal = document.getElementById('html-modal');
-    const htmlEditor = document.getElementById('html-editor');
-    const content = quill.root.innerHTML;
-
-    htmlEditor.value = content;
-    modal.style.display = "block";
-
-    document.getElementById('save-html-button').onclick = function() {
-        const newContent = htmlEditor.value;
-        quill.clipboard.dangerouslyPasteHTML(newContent);
-        modal.style.display = "none";
-    };
-
-    document.querySelector('.close').onclick = function() {
-        modal.style.display = "none";
-    };
-
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    };
+// Función para cargar el contenido del menú en Quill
+function loadMenuContent(content) {
+  quill.clipboard.dangerouslyPasteHTML(content);
 }
+
+// Recibir el contenido del menú desde el servidor
+socket.on('contentUpdate', (data) => {
+  loadMenuContent(data.content);
+});
+
+// Guardar el contenido del menú cuando se hace clic en "Guardar Cambios"
+document.getElementById('save-button').addEventListener('click', () => {
+  const content = quill.root.innerHTML;
+  socket.emit('save', { content });
+});
