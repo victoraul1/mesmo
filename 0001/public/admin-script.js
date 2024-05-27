49b319bdcf003ed992333134ec0476257fb854c4
Victor Galindo <victorgalindo475@gmail.com>
2024-05-27 11:14:40 -0700

    Fix independent functionality for directories 0001 and 0002

--- 0001/public/admin-script.js
+++ 0001/public/admin-script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const socket = io('http://localhost:3001');
+    const socket = io('http://localhost:3001'); // Asegúrate de que apunte al puerto correcto
 
     const quill = new Quill('#editor-container', {
         theme: 'snow',
