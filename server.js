021dec404fbb412a61939ac33c1c7c6596279ab1
Victor Galindo <victorgalindo475@gmail.com>
2024-06-04 23:37:40 -0700

    Updated project structure and files for responsive grid

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
+const app = express();
+const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
-const path = require('path');
 
-const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-// Middleware to determine the correct directory based on the subdomain
 app.use((req, res, next) => {
     let subdomain = req.headers.host.split('.')[0]; // gets 'admi' or 'carta'
     req.restaurantId = subdomain === 'admi' ? 'admin' : 'carta';
@@ -15,23 +14,23 @@ app.use((req, res, next) => {
 });
 
 // Serve static files dynamically from the corresponding public directory
-app.use('/:id/public', express.static((req, res, next) => {
-    const baseDir = path.join(__dirname, req.params.id, 'public');
-    return baseDir;
-}));
+app.use('/:id/public', (req, res, next) => {
+    let baseDir = path.join(__dirname, req.params.id, 'public');
+    console.log('Base directory for static files:', baseDir); // Debugging output
+    express.static(baseDir)(req, res, next);
+});
 
 // Dynamic routing to serve admin.html or index.html based on the subdomain
 app.get('/:id/', (req, res) => {
-    const file = req.restaurantId === 'admin' ? 'admin.html' : 'index.html';
+    let file = req.restaurantId === 'admin' ? 'admin.html' : 'index.html';
     res.sendFile(path.join(__dirname, req.params.id, 'public', file));
 });
 
-// Serving the socket.io client file
+// Path for socket.io client script
 app.get('/socket.io/socket.io.js', (req, res) => {
     res.sendFile(path.resolve('./node_modules/socket.io/client-dist/socket.io.js'));
 });
 
-// Socket.io connection handling
 io.on('connection', (socket) => {
     console.log('A user connected');
     socket.on('disconnect', () => {
@@ -43,4 +42,5 @@ const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
 
